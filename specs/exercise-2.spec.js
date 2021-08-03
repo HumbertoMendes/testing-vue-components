cbfc17fa19617d6f528c5ec8601dda1dd877522c
Humberto Mendes <humberto.mendes@laiv.ly>
2021-08-03 16:29:04 -0500

    + Implemented tests for exercise-2.spec

--- specs/exercise-2.spec.js
+++ specs/exercise-2.spec.js
@@ -1,2 +1,22 @@
 import ExerciseForm from '@/components/exercise-2';
 import { mount } from '@vue/test-utils';
+
+test('follow the user through the form', async () => {
+  const wrapper = mount(ExerciseForm);
+
+  expect(wrapper).toMatchSnapshot();
+
+  const form = wrapper.find('form');
+  const input = form.find('input');
+  const tasksList = wrapper.find('ul');
+
+  input.setValue('my todo');
+  await form.trigger('submit');
+
+  expect(wrapper).toMatchSnapshot();
+
+  const button = tasksList.find('li').find('button');
+  await button.trigger('click');
+
+  expect(wrapper).toMatchSnapshot();
+});
